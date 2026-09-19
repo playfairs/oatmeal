d4102d09a674ab0a8ac39b60d806c05717eb8596
playfairs <root@playfairs.cc>
2026-09-19 11:41:07 -0500

    feat: define SwiftUI settings bridge

---
+++ src/platform/macos/settings/settings_bridge.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void (*OatmealSettingsCallback)(int32_t action, int32_t index, const char *value);
+
+void oatmeal_swift_show_settings(const char *shortcuts, int32_t theme,
+                                 int32_t position, double duration,
+                                 OatmealSettingsCallback callback);
+
+#ifdef __cplusplus
+}
+#endif
