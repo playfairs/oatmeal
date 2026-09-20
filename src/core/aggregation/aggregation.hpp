a2115d2746c682980c966209dfa2e0b76011c1aa
playfairs <root@playfairs.cc>
2026-09-20 02:38:22 -0500

    refactor: move aggregation logic into core/aggregation module

--- src/core/aggregation/aggregation.hpp
+++ src/core/aggregation/aggregation.hpp
@@ -1,26 +1,24 @@
 #pragma once
 
-#include "core/shortcut/shortcut.hpp"
-
 #include <chrono>
 #include <optional>
+
+#include "core/shortcut/shortcut.hpp"
 
 namespace oatmeal {
 
 class TriggerAggregator {
-public:
-  explicit TriggerAggregator(
-      std::chrono::milliseconds window = std::chrono::milliseconds(650))
+ public:
+  explicit TriggerAggregator(std::chrono::milliseconds window = std::chrono::milliseconds(650))
       : window_(window) {}
 
-  int record(const Shortcut &shortcut,
-             std::chrono::steady_clock::time_point now);
+  int record(const Shortcut& shortcut, std::chrono::steady_clock::time_point now);
 
-private:
+ private:
   std::chrono::milliseconds window_;
   std::optional<Shortcut> last_shortcut_;
   std::chrono::steady_clock::time_point last_time_{};
   int count_ = 0;
 };
 
-} // namespace oatmeal
+}  // namespace oatmeal
