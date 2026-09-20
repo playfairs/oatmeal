396547d2f262868032f04be566fdf8f775714b03
playfairs <root@playfairs.cc>
2026-09-20 02:38:22 -0500

    build: add Swift formatter to flake shell

--- flake.nix
+++ flake.nix
@@ -4,20 +4,35 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nox.url = "github:playfairs/nox/v1.2.2-stable";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs = { self, nixpkgs, nox, treefmt-nix }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nox,
+    }:
     let
-      systems = [ "aarch64-darwin" "x86_64-darwin" ];
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f (import nixpkgs { inherit system; }));
-      formatterFor = pkgs: import ./nix/formatter.nix { inherit pkgs treefmt-nix; };
-    in {
+      formatterFor = pkgs: import ./nix/formatter.nix { inherit pkgs; };
+    in
+    {
       formatter = forAllSystems (pkgs: formatterFor pkgs);
 
       devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
-          packages = [ pkgs.clang pkgs.swift pkgs.clang-tools pkgs.csharpier nox.packages.${pkgs.system}.default ];
+          packages = [
+            pkgs.clang
+            pkgs.swift
+            pkgs.clang-tools
+            pkgs.csharpier
+            pkgs.swift-format
+            nox.packages.${pkgs.system}.default
+          ];
           shellHook = ''
             export SDKROOT="$(xcrun --sdk macosx --show-sdk-path 2>/dev/null || true)"
           '';
