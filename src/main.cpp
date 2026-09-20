b62616631e7b49282fca061a2dcb046750fb250c
playfairs <root@playfairs.cc>
2026-09-20 02:27:32 -0500

    refactor: move entry point to platform-neutral main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,3 @@
+#include "platform/macos/application/application.hpp"
+
+int main() { return oatmeal::run(); }
--- src/main.mm
+++
@@ -1,3 +0,0 @@
-#include "platform/macos/application.hpp"
-
-int main() { return oatmeal::run(); }
