f79546a1509c64a94d194a74d0e4404ae6d2b6c8
playfairs <root@playfairs.cc>
2026-09-19 21:54:53 -0500

    build: create macOS app bundle with icon and code signing

--- build_app.sh
+++ build_app.sh
@@ -2,11 +2,16 @@
 set -e
 
 APP_BUNDLE="build/Oatmeal.app"
+rm -rf "$APP_BUNDLE"
 mkdir -p "$APP_BUNDLE/Contents/MacOS"
 mkdir -p "$APP_BUNDLE/Contents/Resources"
 
 cp Info.plist "$APP_BUNDLE/Contents/"
 cp build/debug/oatmeal/oatmeal "$APP_BUNDLE/Contents/MacOS/"
 chmod +x "$APP_BUNDLE/Contents/MacOS/oatmeal"
+
+cp icons/Icon-macOS-Default-1024x1024@1x.icns "$APP_BUNDLE/Contents/Resources/AppIcon.icns"
+
+codesign --force --deep --sign - "$APP_BUNDLE"
 
 echo "App bundle created at $APP_BUNDLE"
